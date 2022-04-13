a502f1ff630b856c154c33d628245ee9448f104a
Kevin Helliwell <kbh789@gmail.com>
2022-04-12 21:15:25 -0400

    spacing

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ func main() {
 	var remainingTickets uint = 50
 	
 	// fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
+	
 	fmt.Printf("Welcome to our %v booking application\n", conferenceName)
 	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
